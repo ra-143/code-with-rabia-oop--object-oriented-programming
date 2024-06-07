3196f8e31aaa2867dc11cda6fb35a47a1f75dcd4
ra-143 <ayesharabia0332@gmail.com>
2024-06-07 21:12:28 +0500

    adding OOP

---
+++ main.js
@@ -0,0 +1,56 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log("=".repeat(80));
+        console.log(chalk.greenBright("Welcome to OOP=Object Oriented Program!"));
+        console.log("=".repeat(80));
+        const ans = await inquirer.prompt({
+            name: "select",
+            type: "list",
+            message: chalk.blue("Whom would you like to interact with?"),
+            choices: ["staff", "student", "exit"]
+        });
+        if (ans.select == "staff") {
+            console.log(chalk.green("You approached the staff room. Please feel,free to ask any question."));
+        }
+        else if (ans.select == "student") {
+            const ans = await inquirer.prompt({
+                name: "student",
+                type: "input",
+                message: chalk.blue("Enter the students's anme you wish to engage with:")
+            });
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello I am ${name.name}. Nice to meet you!`);
+                console.log("New student list:");
+                console.log("Current student list:");
+                console.log(persons.students);
+            }
+            else {
+                console.log(` Hello I am ${Student.name}. Glad to hear you again!`);
+                console.log("Existing student list:");
+                console.log(persons.students);
+            }
+        }
+        else if (ans.select == "exit") {
+            console.log(chalk.red("Exiting the Program"));
+        }
+    } while (true);
+};
+programStart(persons);
